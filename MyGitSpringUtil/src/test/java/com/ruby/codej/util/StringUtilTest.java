6bcc4a3efb7943a19a0493d6003a1e2fceff5625
code-j <darkrubyeye@gmail.com>
2014-03-23 18:59:06 +0900

    isNotEmpty Test

--- MyGitSpringUtil/src/test/java/com/ruby/codej/util/StringUtilTest.java
+++ MyGitSpringUtil/src/test/java/com/ruby/codej/util/StringUtilTest.java
@@ -8,10 +8,20 @@ import org.junit.Test;
 public class StringUtilTest {
 
 	@Test
-	public void test() {
+	public void testIsEmpty() {
 		assertTrue("문자열에 null이 들어올 때", StringUtil.isEmpty(null));
 		assertTrue("문자열에 공백이 들어올 때", StringUtil.isEmpty(""));
 
 		assertFalse("문자열이 있 을때", StringUtil.isEmpty("message!!"));
 	}
+
+
+	@Test
+	public void testIsNotEmpty() {
+		assertFalse("문자열에 null이 들어올 때 실패", StringUtil.isNotEmpty(null));
+		assertFalse("문자열에 공백이 들어올 때 실패", StringUtil.isNotEmpty(""));
+
+		assertTrue("문자열이 있 을때 성공", StringUtil.isNotEmpty("message!!"));
+	}
+
 }
